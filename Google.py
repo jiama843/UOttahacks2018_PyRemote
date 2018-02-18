2635a793e203592f8ae33d75ba8a95378c39927b
Eric Luo <30668005+2017soft@users.noreply.github.com>
2018-02-18 08:59:47 -0500

    Add files via upload

--- Google.py
+++ Google.py
@@ -11,13 +11,6 @@ This file search the inputed key word on Google
 ''' 
 import sys
 import webbrowser
-import pyautogui
-pyautogui.FAILSAFE = True
-search_x , search_y = (450, 437)
 
-webbrowser.open('https://www.google.ca/')
-
-pyautogui.moveTo(search_x, search_y, 1.2)
-pyautogui.doubleClick()
-pyautogui.typewrite(sys.argv[3])
-pyautogui.press('enter')
+url = "http://google.com/?#q="
+webbrowser.open(url + sys.argv[3])
